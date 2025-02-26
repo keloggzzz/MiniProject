1fb06e360281fea00c8ef290dec84e652568c706
keloggzzz <kelis.clark10@gmail.com>
2025-02-25 20:23:15 -0500

    Minor edits to format

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -13,8 +13,11 @@ export default function Home(){
 
 return(
       <div>
-                <h1 className="italic">The Pop Shop</h1>
-                <h2 className="text-2xl text-pink-500">There are currently {length} items available:</h2>
+        <br></br>
+                <div className="text-pink-500 text-left">Welcome to PixelPop! </div>
+                <br></br>
+                <h1 className="italic border-2 rounded-lg">Featured Items</h1>
+                <h2 className="text-xl italic text-pink-500">These {length} items are currently trending :</h2>
                 <br></br>
        
 
